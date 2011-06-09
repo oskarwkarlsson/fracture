14efe5a6cc1df05db62c2adc3cdeb6e60c7a75e3
Ryan Riley <ryan.riley@panesofglass.org>
2011-06-08 22:46:47 -0700

    Add ObjectPool and use it instead of BlockingCollection in BocketPool.

--- src/fracture/BocketPool.fs
+++ src/fracture/BocketPool.fs
@@ -8,23 +8,19 @@ open System.Collections.Concurrent
 type internal BocketPool(name, maxPoolCount, perBocketBufferSize) =
     let totalsize = (maxPoolCount * perBocketBufferSize)
     let buffer = Array.zeroCreate<byte> totalsize
-    let pool = new BlockingCollection<SocketAsyncEventArgs>(maxPoolCount:int)
     let subscriptions = new ConcurrentDictionary<int, IDisposable>()
-    let disposed = ref false
-
     let dispose (args: SocketAsyncEventArgs) =
         let subscription = subscriptions.[args.GetHashCode()]
         subscription.Dispose()
         args.Dispose()
+    let generate() = new SocketAsyncEventArgs()
+    let pool = new ObjectPool<SocketAsyncEventArgs>(maxPoolCount, generate, cleanUp = dispose)
+    let disposed = ref false
 
     let cleanUp disposing = 
         if not !disposed then
             if disposing then
-                pool.CompleteAdding()
-                while pool.Count > 1 do
-                    let args = pool.Take()
-                    dispose args
-                pool.Dispose()
+                pool.Clear()
             disposed := true
 
     let checkedOperation operation onFailure =
@@ -46,7 +42,7 @@ type internal BocketPool(name, maxPoolCount, perBocketBufferSize) =
 
     member this.CheckOut() =
         if not !disposed then
-            checkedOperation pool.Take raiseDisposed
+            checkedOperation pool.Get raiseDisposed
         else raiseDisposed()
 
     member this.CheckIn(args) =
@@ -56,7 +52,7 @@ type internal BocketPool(name, maxPoolCount, perBocketBufferSize) =
             if args.Count < perBocketBufferSize then 
                 args.SetBuffer(args.Offset, perBocketBufferSize)
             // we might be trying to update the the pool when it's already been disposed. 
-            checkedOperation (fun () -> pool.Add(args)) (fun () -> dispose args)
+            checkedOperation (fun () -> pool.Put(args)) (fun () -> dispose args)
         // the pool is kicked, dispose of it ourselves.
         else dispose args
             
