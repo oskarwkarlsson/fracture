5193644cef3a534b934f825e1e466aea2b31d45a
panesofglass <ryan.riley@panesofglass.org>
2011-11-18 23:35:27 -0800

    Add proper Keep-Alive/Connection:Close support

--- samples/TestServer/Program.fs
+++ samples/TestServer/Program.fs
@@ -14,10 +14,22 @@ System.AppDomain.CurrentDomain.UnhandledException |> Observable.add debug
 let shortdate = DateTime.UtcNow.ToShortDateString
 open Fracture.HttpServer
 
-let response = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: Keep-Alive\r\nContent-Length: 12\r\nServer: Fracture\r\n\r\nHello world.\r\n\r\n"
-let server = new HttpServer(headers = (fun (headers, keepAlive, svr, connection, endPoint) -> svr.Send(connection, response, not keepAlive)), 
-                            body = (fun _ -> ()), 
-                            requestEnd = fun _ -> ())
+let onHeaders(headers: HttpRequestHeaders, keepAlive, server: HttpServer, connection, endPoint) =
+    let connectionHeader =
+        if keepAlive then
+            if headers.Version.Minor = 0 then
+                "Connection: Keep-Alive\r\n"
+            else ""
+        else
+            if headers.Version.Minor = 1 then
+                "Connection: Close\r\n"
+            else ""
+    let response = sprintf "HTTP/%d.%d 200 OK\r\nContent-Type: text/plain\r\n%sContent-Length: 12\r\nServer: Fracture\r\n\r\nHello world."
+                           headers.Version.Major
+                           headers.Version.Minor
+                           connectionHeader
+    server.Send(connection, response, not keepAlive)
+let server = new HttpServer(headers = onHeaders, body = ignore, requestEnd = ignore)
 
 server.Start(6667)
 printfn "Http Server started"
