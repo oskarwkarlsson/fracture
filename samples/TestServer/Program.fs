0c2aede5daa6321d6e307dff86a74714475a69f2
Ryan Riley <ryan.riley@panesofglass.com>
2011-11-23 07:04:28 -0800

    Move HttpResponse dsl to Fracture.Http

--- samples/TestServer/Program.fs
+++ samples/TestServer/Program.fs
@@ -5,6 +5,7 @@ open System.Net
 open System.Text
 open Fracture
 open Fracture.Http
+open Fracture.Http.HttpResponse
 
 let debug (x:UnhandledExceptionEventArgs) =
     Console.WriteLine(sprintf "%A" (x.ExceptionObject :?> Exception))
@@ -12,27 +13,6 @@ let debug (x:UnhandledExceptionEventArgs) =
 
 System.AppDomain.CurrentDomain.UnhandledException |> Observable.add debug
 let shortdate = DateTime.UtcNow.ToShortDateString
-
-let status (major, minor) statusCode (sb: StringBuilder) =
-    sb.AppendFormat("HTTP/{0}.{1} {2}", major, minor, statusCode).AppendLine()
-
-let header (key, value) (sb: StringBuilder) = sb.AppendLine(key + ": " + value.ToString())
-
-let connectionHeader minor keepAlive (sb: StringBuilder) =
-    if keepAlive then
-        if minor = 0 then
-            sb |> header ("Connection", "Keep-Alive")
-        else sb
-    else
-        if minor = 1 then
-            sb |> header ("Connection", "Close")
-        else sb
-
-let complete (content: byte[]) (sb: StringBuilder) =
-    sb.AppendLine() |> ignore
-    if content <> null && content.Length > 0 then
-        sb.Append(content).ToString()
-    else sb.ToString()
 
 let onHeaders(headers: HttpRequestHeaders, keepAlive, server: HttpServer, connection, endPoint) =
 
---
+++ src/http/HttpResponse.fs
@@ -0,0 +1,24 @@
+﻿module Fracture.Http.HttpResponse
+
+open System.Text
+
+let status (major, minor) statusCode (sb: StringBuilder) =
+    sb.AppendFormat("HTTP/{0}.{1} {2}", major, minor, statusCode).AppendLine()
+
+let header (key, value) (sb: StringBuilder) = sb.AppendLine(key + ": " + value.ToString())
+
+let connectionHeader minor keepAlive (sb: StringBuilder) =
+    if keepAlive then
+        if minor = 0 then
+            sb |> header ("Connection", "Keep-Alive")
+        else sb
+    else
+        if minor = 1 then
+            sb |> header ("Connection", "Close")
+        else sb
+
+let complete (content: byte[]) (sb: StringBuilder) =
+    sb.AppendLine() |> ignore
+    if content <> null && content.Length > 0 then
+        sb.Append(content).ToString()
+    else sb.ToString()
