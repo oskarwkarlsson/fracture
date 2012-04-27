5311df6608c3ced438d83a3745c5d2184ccc87be
Ryan Riley <ryan.riley@panesofglass.org>
2012-04-27 13:31:28 -0500

    Replace HttpMachine with new HttpParser.

--- samples/TestServer/Program.fs
+++ samples/TestServer/Program.fs
@@ -14,9 +14,7 @@ open Fracture.HttpServer
 
 let data = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: Keep-Alive\r\nContent-Length: 16\r\nServer: Fracture\r\n\r\nHello world.\r\n\r\n"B
 
-let server = new HttpServer( fun (req, res) -> 
-    let result = res data
-    () )
+let server = new HttpServer(fun (req, res) -> res data |> ignore)
 
 server.Start(6667)
 printfn "Http Server started"
