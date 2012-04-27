c37737e3f8866b395071f7950458317ef909c415
Ryan Riley <ryan.riley@panesofglass.org>
2012-04-27 12:38:00 -0500

    Added httpmachine performance test for comparison.

--- src/http/HttpServer.fs
+++ src/http/HttpServer.fs
@@ -5,15 +5,16 @@ open System.Collections.Concurrent
 open System.Collections.Generic
 open System.Diagnostics
 open System.Net
+open System.Net.Http
 open System.Text
 open System.Threading.Tasks
 open Fracture
 open Fracture.Common
 open Fracture.Pipelets
-open Fracture.Http.Core
-open HttpMachine
+open Fracture.Http
+open FSharp.IO
 
-type HttpServer(onRequest) as this = 
+type HttpServer(onRequest) = 
     let disposed = ref false
     let parserCache = new ConcurrentDictionary<_,_>()
 
@@ -25,7 +26,12 @@ type HttpServer(onRequest) as this =
                 parser.Execute(ArraySegment<_>()) |> ignore
 
     let rec svr = TcpServer.Create(received = onReceive, disconnected = onDisconnect)
+
     and createParser endPoint = 
+        let stream = new CircularStream(4096)
+        let parser = new HttpParser()
+        // Kick off the parser and submit the result to the waiting TcpServer.Send.
+        // Return the stream or even stream.Write/stream.AsyncWrite
         HttpParser(ParserDelegate(onHeaders = (fun headers -> (Console.WriteLine(sprintf "Headers: %A" headers.Headers))), //NOTE: on ab.exe without the keepalive option only the headers callback fires
                                   requestBody = (fun body -> (Console.WriteLine(sprintf "Body: %A" body))),
                                   requestEnded = fun req -> onRequest( req, (svr:TcpServer).Send endPoint req.RequestHeaders.KeepAlive) 
@@ -33,21 +39,19 @@ type HttpServer(onRequest) as this =
 
     and onReceive: Func<_,_> = 
         Func<_,_>( fun (endPoint, data) -> Task.Factory.StartNew(fun () ->
-        parserCache.AddOrUpdate(endPoint, createParser endPoint, fun _ value -> value )
-        |> fun parser -> parser.Execute( ArraySegment(data) ) |> ignore))
+          parserCache.AddOrUpdate(endPoint, createParser endPoint, fun _ value -> value )
+          |> fun parser -> parser.Execute( ArraySegment(data) ) |> ignore))
+        
+    member h.Start(port) = svr.Listen(IPAddress.Loopback, port)
 
-    
-    
     //ensures the listening socket is shutdown on disposal.
-    let cleanUp disposing = 
+    member private this.Dispose(disposing) = 
         if not !disposed then
             if disposing && svr <> Unchecked.defaultof<TcpServer> then
                 (svr :> IDisposable).Dispose()
             disposed := true
-        
-    member h.Start(port) = svr.Listen(IPAddress.Loopback, port)
 
     interface IDisposable with
-        member h.Dispose() =
-            cleanUp true
+        member this.Dispose() =
+            this.Dispose(true)
             GC.SuppressFinalize(this)
