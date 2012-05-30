ca7960f5ad6b90b15315d55b61c27f3f722befa8
Ryan Riley <ryan.riley@panesofglass.org>
2012-05-30 14:07:02 -0500

    Manage state correctly.

--- src/http/HttpParser.fs
+++ src/http/HttpParser.fs
@@ -11,9 +11,9 @@ open FSharp.IO
 open FSharpx
 
 type private ParserState =
-  | StartLine of Stream * HttpRequestMessage
-  | Headers of Stream * HttpRequestMessage
-  | Body of int64 * Stream * HttpRequestMessage
+  | StartLine of byte list * Stream * HttpRequestMessage
+  | Headers of byte list * Stream * HttpRequestMessage
+  | Body of byte list * int64 * Stream * HttpRequestMessage
   | Done of HttpRequestMessage
 
 type private EPSDictionary = System.Collections.Generic.Dictionary<EndPoint, ParserState>
@@ -35,48 +35,54 @@ type HttpParser(cont) =
 
     let agent = Agent.Start(fun inbox ->
         let states = new EPSDictionary()
-        let rec step acc = async {
+        let rec step () = async {
             let! (endPoint, chunk: ArraySegment<byte>) = inbox.Receive()
             if chunk.Count > 0 then
                 let state =
                     if not <| states.ContainsKey(endPoint) then
                         let stream = new MemoryStream()
-                        StartLine(stream, new HttpRequestMessage(Content = new StreamContent(stream)))
+                        StartLine([], stream, new HttpRequestMessage(Content = new StreamContent(stream)))
                     else states.[endPoint]
-                let rec loop bs i state =
-                    if i = chunk.Count then bs, state else
+                let rec loop i state =
+                    if i = chunk.Count then state else
                     let c = chunk.Array.[i + chunk.Offset]
                     if c = '\r'B then
-                        let state' = HttpParser.UpdateRequest(bs, state)
+                        let state' = HttpParser.UpdateRequest state
                         let j = i + 1
                         if j < chunk.Count && chunk.Array.[j + chunk.Offset] = '\n'B then
-                            loop [] (j + 1) state'
-                        else loop [] j state'
+                            loop (j + 1) state'
+                        else loop j state'
                     elif c = '\n'B then
-                        let state' = HttpParser.UpdateRequest(bs, state)
-                        loop [] (i + 1) state'
+                        let state' = HttpParser.UpdateRequest state
+                        loop (i + 1) state'
                     else
-                        loop (c :: bs) (i + 1) state
-                let acc', state' = loop [] 0 state
+                        let state' =
+                            match state with
+                            | StartLine(bs, stream, request) -> StartLine(c::bs, stream, request)
+                            | Headers(bs, stream, request) -> Headers(c::bs, stream, request)
+                            | Body(bs, bytesRead, stream, request) -> Body(c::bs, bytesRead, stream, request)
+                            | _ -> state
+                        loop (i + 1) state'
+                let state' = loop 0 state
                 match state' with
                 | Done request ->
                     cont(endPoint, request)
                     if states.ContainsKey(endPoint) then states.Remove(endPoint) |> ignore
                 | _ -> states.[endPoint] <- state'
-                return! step acc'
+                return! step ()
             else
                 if states.ContainsKey(endPoint) then
                     match states.[endPoint] with
                     | Done _ ->
                         states.Remove(endPoint) |> ignore
                     | state ->
-                        match HttpParser.UpdateRequest([], state) with
+                        match HttpParser.UpdateRequest state with
                         | Done request ->
                             cont(endPoint, request)
                             states.Remove(endPoint) |> ignore
                         | _ -> () // the socket may have paused. We should probably add a timeout that is reset on each received packet.
-                return! step [] }
-        step [] )
+                return! step () }
+        step () )
 
     member x.Post(endPoint, data) = agent.Post (endPoint, data)
 
@@ -105,31 +111,31 @@ type HttpParser(cont) =
 
     static member private IsContentHeader(name) = Array.exists ((=) name) contentHeaders
 
-    static member private UpdateRequest (bs, state) =
+    static member private UpdateRequest state =
         match state with
-        | StartLine (stream, request) ->
+        | StartLine (bs, stream, request) ->
             let block = bs |> List.rev |> List.toArray
             let line = Encoding.ASCII.GetString(block)
             HttpParser.ParseRequestLine(line, request)
-            Headers (stream, request)
-        | Headers (stream, request) ->
+            Headers ([], stream, request)
+        | Headers (bs, stream, request) ->
             match bs with
             | [] ->
                 if request.Method = HttpMethod.Get || request.Method = HttpMethod.Head then
                     // TODO: This isn't technically correct, but we need to fix this later.
                     Done request
-                else Body (0L, stream, request)
+                else Body ([], 0L, stream, request)
             | _ ->
                 let block = bs |> List.rev |> List.toArray
                 let line = Encoding.ASCII.GetString(block)
                 HttpParser.ParseHeader(line, request)
-                Headers (stream, request)
-        | Body (bytesRead, stream, request) ->
+                Headers ([], stream, request)
+        | Body (bs, bytesRead, stream, request) ->
             HttpParser.WriteToStream (bs, stream)
             let bytesRead = bytesRead + int64 bs.Length
             if bs.Length = 0 || (request.Content.Headers.ContentLength.HasValue && bytesRead = request.Content.Headers.ContentLength.Value) then
                 Done request
-            else Body (bytesRead, stream, request)
+            else Body ([], bytesRead, stream, request)
         | s -> s
 
     static member private WriteToStream (bs: byte list, stream: Stream) =
