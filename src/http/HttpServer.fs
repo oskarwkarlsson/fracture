fb8561c6088a28577471fe8a8be87ba6b51fe781
Ryan Riley <ryan.riley@panesofglass.org>
2013-07-14 22:08:32 -0500

    Exchange callbacks for events/observables.

--- src/http/HttpServer.fs
+++ src/http/HttpServer.fs
@@ -28,16 +28,17 @@ open Fracture
 open Fracture.Common
 open HttpMachine
 
+[<Sealed>]
 type HttpServer(headers, body, requestEnd) as this = 
-    let mutable disposed = false
-
-    let svr = TcpServer.Create((fun (data,svr,sd) -> 
-        let parser =
-            let parserDelegate = ParserDelegate(onHeaders = (fun h -> headers(h,this,sd)), 
-                                                requestBody = (fun data -> (body(data, svr,sd))), 
-                                                requestEnded = (fun req -> (requestEnd(req, svr, sd))))
-            HttpParser(parserDelegate)
-        parser.Execute(new ArraySegment<_>(data)) |> ignore))
+    let svr = TcpServer.Create()
+    let receivedSubscription =
+        svr.OnReceived.Subscribe((fun (svr, (data, sd)) -> 
+            let parser =
+                let parserDelegate = ParserDelegate(onHeaders = (fun h -> headers(h,this,sd)), 
+                                                    requestBody = (fun data -> (body(data, svr,sd))), 
+                                                    requestEnded = (fun req -> (requestEnd(req, svr, sd))))
+                HttpParser(parserDelegate)
+            parser.Execute(new ArraySegment<_>(data)) |> ignore))
         
     member h.Start(port) = svr.Listen(IPAddress.Loopback, port)
 
@@ -45,15 +46,10 @@ type HttpServer(headers, body, requestEnd) as this =
         let encoded = Encoding.ASCII.GetBytes(response)
         svr.Send(client, encoded, keepAlive)
 
-    //ensures the listening socket is shutdown on disposal.
-    member private x.Dispose(disposing) = 
-        if not disposed then
-            if disposing && svr <> Unchecked.defaultof<TcpServer> then
-                svr.Dispose()
-            disposed <- true
-
+    /// Ensures the listening socket is shutdown on disposal.
     member h.Dispose() =
-        h.Dispose(true)
+        receivedSubscription.Dispose()
+        svr.Dispose()
         GC.SuppressFinalize(this)
 
     interface IDisposable with
